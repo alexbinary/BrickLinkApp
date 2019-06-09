45b2a3947b5daf9b420e5b3f2785524d358de530
Alexandre Bintz <alexandre@bintz.io>
2019-06-09 17:57:32 +0200

    Initial Commit

---
+++ BrickLinkApp2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BrickLinkApp2
+//
+//  Created by Alexandre Bintz on 09/06/2019.
+//  Copyright © 2019 Alexandre Bintz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

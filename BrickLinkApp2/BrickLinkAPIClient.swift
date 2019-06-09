d45771ecb0955e0c76bfe4237c2b9017ffc568f0
Alexandre Bintz <alexandre@bintz.io>
2019-06-09 19:14:06 +0200

    BrickLink API client stub

---
+++ BrickLinkApp2/BrickLinkAPIClient.swift
@@ -0,0 +1,19 @@
+//
+//  BrickLinkAPIClient.swift
+//  BrickLinkApp2
+//
+//  Created by Alexandre Bintz on 09/06/2019.
+//  Copyright © 2019 Alexandre Bintz. All rights reserved.
+//
+
+import Foundation
+
+
+struct BrickLinkAPIClient {
+    
+    
+    func getOrders() {
+        
+        print("get orders")
+    }
+}

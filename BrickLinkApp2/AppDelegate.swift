b5a6331ea0d26e56ed1fd8a2e4433f6231736677
Alexandre Bintz <alexandre@bintz.io>
2019-06-14 17:51:32 +0200

    Cleanup credentials loading

--- BrickLinkApp2/AppDelegate.swift
+++ BrickLinkApp2/AppDelegate.swift
@@ -2,16 +2,14 @@
 import UIKit
 
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let url = Bundle.main.url(forResource: "BrickLinkCredentials", withExtension: "plist")!
-        
-        let data = try! Data(contentsOf: url)
-        
-        let credentials = try! PropertyListDecoder().decode(BrickLinkRequestCredentials.self, from: data)
+        let credentials = loadBrickLinkCredentials()
         
         let client = BrickLinkAPIClient(with: credentials)
         
@@ -19,5 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    
+    func loadBrickLinkCredentials() -> BrickLinkRequestCredentials {
+        
+        let url = Bundle.main.url(forResource: "BrickLinkCredentials", withExtension: "plist")!
+        
+        let data = try! Data(contentsOf: url)
+        
+        let credentials = try! PropertyListDecoder().decode(BrickLinkRequestCredentials.self, from: data)
+        
+        return credentials
+    }
 }
-

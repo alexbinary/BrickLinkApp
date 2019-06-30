99cd5bb9d2004da5d4c5a92161d6e4c873394880
Alexandre Bintz <alexandre@bintz.io>
2019-06-30 08:51:37 +0200

    test: clean template code

--- BrickLinkApp2Tests/BrickLinkApp2Tests.swift
+++ BrickLinkApp2Tests/BrickLinkApp2Tests.swift
@@ -1,33 +1,8 @@
-//
-//  BrickLinkApp2Tests.swift
-//  BrickLinkApp2Tests
-//
-//  Created by Alexandre Bintz on 30/06/2019.
-//  Copyright © 2019 Alexandre Bintz. All rights reserved.
-//
 
 import XCTest
 
+
 class BrickLinkApp2Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
